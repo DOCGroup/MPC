45421650eeb9fec3e2c9d2ae79be3b92c9007bb1
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2004-04-21 13:51:14 +0000

    ChangeLogTag: Wed Apr 21 08:50:01 2004  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/modules/VC8WorkspaceCreator.pm
+++ trunk/modules/VC8WorkspaceCreator.pm
@@ -40,45 +40,4 @@ sub pre_workspace {
 }
 
 
-sub print_inner_project {
-  my($self)  = shift;
-  my($fh)    = shift;
-  my($gen)   = shift;
-  my($pguid) = shift;
-  my($deps)  = shift;
-  my($project_name) = shift;
-  my($name_to_guid_map) = shift;
-
-  my($crlf) = $self->crlf();
-  print $fh "\tProjectSection(ProjectDependencies) = postProject$crlf";
-  my($darr) = $self->create_array($deps);
-  foreach my $dep (@$darr) {
-    ## Avoid cirular dependencies
-    if ($project_name ne $dep) {
-      my($guid) = $name_to_guid_map->{$dep};
-      if (defined $guid) {
-        print $fh "\t\t{$guid} = {$guid}$crlf";
-      }
-    }
-  }
-  print $fh "\tEndProjectSection$crlf";
-}
-
-
-sub print_configs {
-  my($self)    = shift;
-  my($fh)      = shift;
-  my($configs) = shift;
-  my($crlf)    = $self->crlf();
-  foreach my $key (sort keys %$configs) {
-    print $fh "\t\t$key = $key$crlf";
-  }
-}
-
-
-sub print_dependencies {
-  ## These are done in the print_inner_project method
-}
-
-
 1;
