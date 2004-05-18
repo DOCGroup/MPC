860ffdcd1ce707e4e18d3d15150bbb3a07e161d4
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2004-05-18 14:39:41 +0000

    ChangeLogTag: Tue May 18 09:37:29 2004  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/modules/Version.pm
+++ trunk/modules/Version.pm
@@ -16,7 +16,30 @@ use strict;
 # Data Section
 # ************************************************************
 
-my($version) = '3.1.1';
+## This is the starting major and minor version
+my($version) = '3.1';
+
+## Here we determine the beta version.  The base variable
+## is the negated number of existing ChangeLog entries at the
+## time of the release of the major and minor version.  We then
+## add the total number of ChangeLog entries to the base to
+## get the beta version.
+my($base) = -36;
+if (open(CLH, ::getBasePath() . '/ChangeLog')) {
+  while(<CLH>) {
+    if (/^\w\w\w\s/) {
+      ++$base;
+    }
+  }
+  close(CLH);
+
+  ## We then append the beta version number to the version string
+  $version .= ".$base";
+}
+else {
+  print "WARNING: Unable to determine the beta version number of MPC\n";
+}
+
 
 # ************************************************************
 # Subroutine Section
