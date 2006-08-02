6b881bc242e1e11e28c5f4552d1e02e251590b12
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2006-08-02 17:45:32 +0000

    Tagging as MPC_3_4_2

---
+++ tags/MPC_3_4_2/modules/VC6ProjectCreator.pm
@@ -0,0 +1,82 @@
+package VC6ProjectCreator;
+
+# ************************************************************
+# Description   : A VC6 Project Creator
+# Author        : Chad Elliott
+# Create Date   : 3/14/2002
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+
+use ProjectCreator;
+use VCProjectBase;
+
+use vars qw(@ISA);
+@ISA = qw(VCProjectBase ProjectCreator);
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+sub project_file_extension {
+  #my($self) = shift;
+  return '.dsp';
+}
+
+
+sub override_valid_component_extensions {
+  my($self)  = shift;
+  my($comp)  = shift;
+  my($array) = undef;
+
+  if ($comp eq 'source_files' && $self->get_language() eq 'cplusplus') {
+    $array = ["\\.cpp", "\\.cxx", "\\.c"];
+  }
+
+  return $array;
+}
+
+
+sub override_exclude_component_extensions {
+  my($self)  = shift;
+  my($comp)  = shift;
+  my($array) = undef;
+
+  if ($comp eq 'source_files') {
+    my(@exts) = ("_T\\.cpp", "_T\\.cxx");
+    $array = \@exts;
+  }
+
+  return $array;
+}
+
+
+sub get_dll_exe_template_input_file {
+  #my($self) = shift;
+  return 'vc6dspdllexe';
+}
+
+
+sub get_lib_exe_template_input_file {
+  #my($self) = shift;
+  return 'vc6dsplibexe';
+}
+
+
+sub get_lib_template_input_file {
+  #my($self) = shift;
+  return 'vc6dsplib';
+}
+
+
+sub get_dll_template_input_file {
+  #my($self) = shift;
+  return 'vc6dspdll';
+}
+
+
+1;
