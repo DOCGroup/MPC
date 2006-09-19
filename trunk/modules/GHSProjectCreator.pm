6ececc64c41a845db2ba95b6caca9902cdb516a4
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2006-09-19 13:07:53 +0000

    ChangeLogTag: Tue Sep 19 13:04:15 UTC 2006  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/modules/GHSProjectCreator.pm
+++ trunk/modules/GHSProjectCreator.pm
@@ -1,7 +1,10 @@
 package GHSProjectCreator;
 
 # ************************************************************
-# Description   : Not a complete implementation for GHS
+# Description   : A GHS project creator for version 4.x.
+#                 By default, this module assumes Multi will
+#                 be used on Windows.  If it is not, you must
+#                 set the MPC_GHS_UNIX environment variable.
 # Author        : Chad Elliott
 # Create Date   : 4/19/2002
 # ************************************************************
@@ -22,10 +25,45 @@ use vars qw(@ISA);
 # ************************************************************
 
 my($startre) = undef;
+my($ghsunix) = 'MPC_GHS_UNIX';
 
 # ************************************************************
 # Subroutine Section
 # ************************************************************
+
+sub convert_slashes {
+  return (defined $ENV{$ghsunix} ? 0 : 1);
+}
+
+
+sub case_insensitive {
+  return (defined $ENV{$ghsunix} ? 0 : 1);
+}
+
+
+sub use_win_compatibility_commands {
+  return (defined $ENV{$ghsunix} ? 0 : 1);
+}
+
+
+sub post_file_creation {
+  my($self) = shift;
+
+  ## These special files are only used if it is a custom only project or
+  ## there are no source files in the project.
+  if ((defined $self->get_assignment('custom_only') ||
+       !defined $self->get_assignment('source_files')) &&
+      defined $self->get_assignment('custom_types')) {
+    my($fh) = new FileHandle();
+    if (open($fh, ">.custom_build_rule")) {
+      print $fh ".empty_html_file\n";
+      close($fh);
+    }
+    if (open($fh, ">.empty_html_file")) {
+      close($fh);
+    }
+  }
+}
 
 sub compare_output {
   #my($self) = shift;
@@ -35,7 +73,7 @@ sub compare_output {
 
 sub project_file_extension {
   #my($self) = shift;
-  return '.bld';
+  return '.gpj';
 }
 
 
@@ -68,6 +106,16 @@ sub fill_value {
     $value =~ s/^$startre[\/]?//;
     if ($value eq '') {
       $value = '.';
+    }
+  }
+  elsif ($name eq 'slash') {
+    $value = (defined $ENV{$ghsunix} ? '/' : '\\');
+  }
+  else {
+    if (!defined $ENV{$ghsunix}) {
+      if ($name eq 'postmkdir') {
+        $value = ' || type nul';
+      }
     }
   }
 
