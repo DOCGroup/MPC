b9cf9401d590df0c3cb3f65d4b98a311dd9b36bc
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2006-11-29 19:08:48 +0000

    ChangeLogTag: Wed Nov 29 19:07:38 UTC 2006  Chad Elliott  <elliott_c@ociweb.com>

--- modules/MPC.pm
+++ modules/MPC.pm
@@ -25,7 +25,6 @@ sub new {
                                       'VC7ProjectCreator',
                                       'VC71ProjectCreator',
                                       'VC8ProjectCreator',
-                                      'VC8WebProjectCreator',
                                       'BDSProjectCreator',
                                       'GHSProjectCreator',
                                       'EM3ProjectCreator',
