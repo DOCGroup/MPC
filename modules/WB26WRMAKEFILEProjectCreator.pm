24562e8312363a37ff959087389c6df727e1bd02
Johnny Willemsen <jwillemsen@remedy.nl>
2008-01-21 06:26:33 +0000

    Mon Jan 21 06:25:12 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- modules/WB26WRMAKEFILEProjectCreator.pm
+++ modules/WB26WRMAKEFILEProjectCreator.pm
@@ -1,4 +1,4 @@
-package WB26WorkspaceCreator;
+package WB26WRMAKEFILEProjectCreator;
 
 # ************************************************************
 # Description   : Workbench 2.6 / VxWorks 6.4 generator
@@ -12,19 +12,30 @@ package WB26WorkspaceCreator;
 
 use strict;
 
-use WB26ProjectCreator;
-use WinWorkspaceBase;
-use WorkspaceCreator;
+use ProjectCreator;
+use WinProjectBase;
+use XMLProjectBase;
 
 use vars qw(@ISA);
-@ISA = qw(WinWorkspaceBase WorkspaceCreator);
+@ISA = qw(XMLProjectBase ProjectCreator);
 
 # ************************************************************
 # Subroutine Section
 # ************************************************************
 
-sub write_workspace {
-  return 1;
+sub crlf {
+  #my($self) = shift;
+  return "\n";
+}
+
+sub project_file_extension {
+  #my($self) = shift;
+  return '/.wrmakefile';
+}
+
+sub get_template {
+  #my($self) = shift;
+  return 'wb26wrmakefile';
 }
 
 1;
