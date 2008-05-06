163a3e9a11c18e09dd8d2cbf0bf559aa0c611da8
Johnny Willemsen <jwillemsen@remedy.nl>
2008-05-06 07:12:31 +0000

    Tue May  6 07:12:10 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- modules/WB26WorkspaceCreator.pm
+++ modules/WB26WorkspaceCreator.pm
@@ -69,6 +69,21 @@ sub write_comps {
   print $fh $all, $self->crlf();
 }
 
+sub post_workspace {
+  my($self)    = shift;
+  my($fh)      = shift;
+  my($creator) = shift;
+  my($crlf)    = $self->crlf();
+  my $pjs  = $self->get_project_info();
+  my @list = $self->sort_dependencies($self->get_projects(), 0);
+  my $all  = 'description.buildorder=';
+
+  foreach my $project ($self->sort_dependencies($self->get_projects(), 0)) {
+    print $fh "$project$crlf";
+    $self->add_dependencies($creator, $project);
+  }
+}
+
 sub get_additional_output {
   ## Create the accompanying list file.  It always goes in the same
   ## directory as the first workspace output file.
@@ -97,5 +112,56 @@ sub list_file_body {
                 '/.project', $crlf;
   }
 }
+
+sub add_dependencies {
+  my($self)    = shift;
+  my($creator) = shift;
+  my($proj)    = shift;
+  my($fh)      = new FileHandle();
+  my($outdir)  = $self->get_outdir();
+  $outdir      = $self->getcwd() if ($outdir eq '.');
+
+  if (open($fh, "$outdir/$proj")) {
+    my($write) = 0;
+    my(@read)  = ();
+    my($cwd)   = $self->getcwd();
+    while(<$fh>) {
+      if (/MPC\s+ADD\s+DEPENDENCIES/) {
+        my(@projs) = ();
+        my($crlf)  = $self->crlf();
+        my($deps)  = $self->get_validated_ordering($proj);
+        foreach my $dep (@$deps) {
+          my($relative) = $self->get_relative_dep_file($creator,
+                                                       "$cwd/$proj",
+                                                       $dep);
+          if (defined $relative) {
+            if (!$write) {
+              $write = 1;
+            }
+            push(@read, "            <subproject project=\"/$dep\"/>$crlf");
+            push(@projs, $relative);
+          }
+        }
+        if ($write) {
+        }
+        else {
+          last;
+        }
+      }
+      else {
+        push(@read, $_);
+      }
+    }
+    close($fh);
+
+    if ($write && open($fh, ">$outdir/$proj")) {
+      foreach my $line (@read) {
+        print $fh $line;
+      }
+      close($fh);
+    }
+  }
+}
+
 
 1;
