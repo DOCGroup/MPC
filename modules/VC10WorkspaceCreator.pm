1adf09945aa2230bf0ac5bec980d544bc91e942f
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-10 10:49:39 +0000

    Mon Nov 10 10:45:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- modules/VC10WorkspaceCreator.pm
+++ modules/VC10WorkspaceCreator.pm
@@ -36,7 +36,7 @@ sub pre_workspace {
   my $crlf = $self->crlf();
 
   print $fh '﻿', $crlf,
-            'Microsoft Visual Studio Solution File, Format Version 10.00', $crlf;
+            'Microsoft Visual Studio Solution File, Format Version 11.00', $crlf;
   $self->print_workspace_comment($fh,
             '# Visual Studio 10', $crlf,
             '# $Id$', $crlf,
