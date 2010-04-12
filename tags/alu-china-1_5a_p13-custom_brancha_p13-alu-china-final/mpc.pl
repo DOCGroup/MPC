ac42c85480ac28f58bb0bf55ee5d9888792de862
cleeland <cleeland@a3e5c962-4219-0410-a828-e124f845ac39>
2010-04-12 16:25:13 +0000

    Tagging as alu-china-1_5a_p13-custom_brancha_p13-alu-china-final

---
+++ tags/alu-china-1_5a_p13-custom_brancha_p13-alu-china-final/mpc.pl
@@ -0,0 +1,47 @@
+#! /usr/bin/perl
+eval '(exit $?0)' && eval 'exec perl -w -S $0 ${1+"$@"}'
+    & eval 'exec perl -w -S $0 $argv:q'
+    if 0;
+
+# ******************************************************************
+#      Author: Chad Elliott
+#        Date: 6/17/2002
+#         $Id$
+# ******************************************************************
+
+# ******************************************************************
+# Pragma Section
+# ******************************************************************
+
+require 5.006;
+
+use strict;
+use FindBin;
+use File::Spec;
+use File::Basename;
+
+## Sometimes $FindBin::RealBin will end up undefined.  If it is, we need
+## to use the directory of the built-in script name.  And, for VMS, we
+## have to convert that into a UNIX path so that Perl can use it
+## internally.
+my $basePath = (defined $FindBin::RealBin && $FindBin::RealBin ne '' ?
+                  $FindBin::RealBin : File::Spec->rel2abs(dirname($0)));
+$basePath = VMS::Filespec::unixify($basePath) if ($^O eq 'VMS');
+unshift(@INC, $basePath . '/modules');
+
+require Driver;
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+sub getBasePath {
+  return $basePath;
+}
+
+# ************************************************************
+# Main Section
+# ************************************************************
+
+my $driver = new Driver($basePath, Driver::projects());
+exit($driver->run(@ARGV));
