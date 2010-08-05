27bbf37d10e29f9590aceca46e0e69ea4df3f389
elliott_c <elliott_c@a3e5c962-4219-0410-a828-e124f845ac39>
2010-08-05 03:50:05 +0000

    ChangeLogTag: Thu Aug  5 03:49:19 UTC 2010  Chad Elliott  <elliott_c@ociweb.com>

--- modules/CDT6WorkspaceCreator.pm
+++ modules/CDT6WorkspaceCreator.pm
@@ -56,7 +56,7 @@ sub pre_workspace {
     '# The projects are listed in dependency order.', $crlf,
     '#', $crlf,
     '# MPC Command:', $crlf,
-    "# $0 @ARGV", $crlf,
+    '# ', $self->create_command_line_string($0, @ARGV), $crlf,
     '#----------------------------------------------------------------------------', $crlf);
 }
 
