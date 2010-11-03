f16dc448839b04750149cc4a6b69407f00041ce5
calkinsc <calkinsc@a3e5c962-4219-0410-a828-e124f845ac39>
2010-11-03 14:55:16 +0000

    Wed Nov  3 14:53:37 UTC 2010  Charles Calkins  <calkinsc@ociweb.com>
            * docs/templates/wix.txt:
            * modules/CDT6WorkspaceCreator.pm:
            * modules/TemplateParser.pm:
            * modules/VC7WorkspaceCreator.pm:
            * modules/WixProjectCreator.pm:
            * modules/WixWorkspaceCreator.pm:
            * templates/wix.mpd:
            * templates/wix.mpt:
            * templates/wixcs.mpd:
            * templates/wixcs.mpt:
            
              Extended the "wix" project type to support C#, decorated names, 
              internationalized .NET resources, and generation of components 
              and groups based on configurations and platforms.

--- modules/WixProjectCreator.pm
+++ modules/WixProjectCreator.pm
@@ -2,8 +2,7 @@ package WixProjectCreator;
 
 # ************************************************************
 # Description   : A Wix Project Creator
-# Author        : James H. Hill
-# Create Date   : 6/23/2009
+# Author        : James H. Hill / Charles Calkins
 # ************************************************************
 
 # ************************************************************
@@ -20,8 +19,40 @@ use vars qw(@ISA);
 @ISA = qw(XMLProjectBase WinProjectBase ProjectCreator);
 
 # ************************************************************
+# Data Section
+# ************************************************************
+
+my %info = (Creator::cplusplus => {'ext'      => '.wxi',
+                                   'dllexe'   => 'wix',
+                                   'libexe'   => 'wix',
+                                   'dll'      => 'wix',
+                                   'lib'      => 'wix',
+                                   'template' => 'wix',
+                                  },
+            Creator::csharp => {'ext'      => '.wxi',
+                                'dllexe'   => 'wixcs',
+                                'libexe'   => 'wixcs',
+                                'dll'      => 'wixcs',
+                                'lib'      => 'wixcs',
+                                'template' => 'wixcs',
+                               },
+           );
+
+# ************************************************************
 # Subroutine Section
 # ************************************************************
+
+sub languageSupported {
+  return defined $info{$_[0]->get_language()};
+}
+
+sub convert_all_variables {
+  return 1;
+}
+
+sub requires_forward_slashes {
+  return 1;
+}
 
 sub expand_variables_from_template_values {
   return 1;
@@ -34,6 +65,54 @@ sub warn_useless_project {
 sub convert_slashes {
   return 0;
 }
+
+
+sub is_culture_code {
+  my $culture_code = shift;
+
+  # from http://sharpertutorials.com/list-of-culture-codes/
+  my @culture_codes = (
+	'af', 'hu-HU', 'af-ZA', 'is', 'sq', 'is-IS',
+	'sq-AL', 'id', 'ar', 'id-ID', 'ar-DZ', 'it',
+	'ar-BH', 'it-IT', 'ar-EG', 'it-CH', 'ar-IQ', 'ja',
+	'ar-JO', 'ja-JP', 'ar-KW', 'kn', 'ar-LB', 'kn-IN',
+	'ar-LY', 'kk', 'ar-MA', 'kk-KZ', 'ar-OM', 'kok',
+	'ar-QA', 'kok-IN', 'ar-SA', 'ko', 'ar-SY', 'ko-KR',
+	'ar-TN', 'ky', 'ar-AE', 'ky-KG', 'ar-YE', 'lv',
+	'hy', 'lv-LV', 'hy-AM', 'lt', 'az', 'lt-LT',
+	'az-AZ-Cyrl', 'mk', 'az-AZ-Latn', 'mk-MK', 'eu', 'ms',
+	'eu-ES', 'ms-BN', 'be', 'ms-MY', 'be-BY', 'mr',
+	'bg', 'mr-IN', 'bg-BG', 'mn', 'ca', 'mn-MN',
+	'ca-ES', 'no', 'zh-HK', 'nb-NO', 'zh-MO', 'nn-NO',
+	'zh-CN', 'pl', 'zh-CHS', 'pl-PL', 'zh-SG', 'pt',
+	'zh-TW', 'pt-BR', 'zh-CHT', 'pt-PT', 'hr', 'pa',
+	'hr-HR', 'pa-IN', 'cs', 'ro', 'cs-CZ', 'ro-RO',
+	'da', 'ru', 'da-DK', 'ru-RU', 'div', 'sa',
+	'div-MV', 'sa-IN', 'nl', 'sr-SP-Cyrl', 'nl-BE', 'sr-SP-Latn',
+	'nl-NL', 'sk', 'en', 'sk-SK', 'en-AU', 'sl',
+	'en-BZ', 'sl-SI', 'en-CA', 'es', 'en-CB', 'es-AR',
+	'en-IE', 'es-BO', 'en-JM', 'es-CL', 'en-NZ', 'es-CO',
+	'en-PH', 'es-CR', 'en-ZA', 'es-DO', 'en-TT', 'es-EC',
+	'en-GB', 'es-SV', 'en-US', 'es-GT', 'en-ZW', 'es-HN',
+	'et', 'es-MX', 'et-EE', 'es-NI', 'fo', 'es-PA',
+	'fo-FO', 'es-PY', 'fa', 'es-PE', 'fa-IR', 'es-PR',
+	'fi', 'es-ES', 'fi-FI', 'es-UY', 'fr', 'es-VE',
+	'fr-BE', 'sw', 'fr-CA', 'sw-KE', 'fr-FR', 'sv',
+	'fr-LU', 'sv-FI', 'fr-MC', 'sv-SE', 'fr-CH', 'syr',
+	'gl', 'syr-SY', 'gl-ES', 'ta', 'ka', 'ta-IN',
+	'ka-GE', 'tt', 'de', 'tt-RU', 'de-AT', 'te',
+	'de-DE', 'te-IN', 'de-LI', 'th', 'de-LU', 'th-TH',
+	'de-CH', 'tr', 'el', 'tr-TR', 'el-GR', 'uk',
+	'gu', 'uk-UA', 'gu-IN', 'ur', 'he', 'ur-PK',
+	'he-IL', 'uz', 'hi', 'uz-UZ-Cyrl', 'hi-IN', 'uz-UZ-Latn',
+	'hu', 'vi');
+
+  return 1 if (exists {map { $_ => 1 } @culture_codes}->{$culture_code});
+  return 0;
+}
+
+
+
 
 sub fill_value {
   my($self, $name) = @_;
@@ -81,40 +160,73 @@ sub fill_value {
 
     return $source;
   }
+  elsif ($name eq 'cultures') {
 
+    my $crlf = $self->crlf();
+
+    # iterate over resx_files, make list of culture abbreviations
+    my @resx_files  = $self->get_component_list('resx_files');
+
+    my %cultures = ();
+    foreach my $resx_file (@resx_files) {
+      my @parts = split('\.', $resx_file);
+      if ($parts[-1] eq 'resx') {  # if the file is a .resx file
+        if (is_culture_code($parts[-2])) { # if a culture is specified
+          $cultures{$parts[-2]} = 1;  # remember that culture
+        }
+        else {
+          $cultures{'_neutral_'} = 1;  # have a neutral culture
+        }
+      }
+    }
+
+    # flatten into a string
+    my $found_cultures = '';
+    foreach my $culture (keys %cultures) {
+      $found_cultures = $found_cultures . $culture . ' ';
+    }
+
+    return $found_cultures;
+  }
   return undef;
 }
 
+
+sub get_info_hash {
+  my($self, $key) = @_;
+
+  ## If we have the setting in our information map, the use it.
+  return $info{$key} if (defined $info{$key});
+}
+
 sub project_file_extension {
-  return '.wxi';
+  return $_[0]->get_info_hash($_[0]->get_language())->{'ext'};
 }
 
 
 sub get_dll_exe_template_input_file {
-  #my $self = shift;
-  return 'wix';
+  return $_[0]->get_info_hash($_[0]->get_language())->{'dllexe'};
 }
 
 
 sub get_lib_exe_template_input_file {
-  #my $self = shift;
-  return 'wix';
-}
-
-
-sub get_lib_template_input_file {
-  #my $self = shift;
-  return 'wix';
+  return $_[0]->get_info_hash($_[0]->get_language())->{'libexe'};
 }
 
 
 sub get_dll_template_input_file {
-  #my $self = shift;
-  return 'wix';
+  return $_[0]->get_info_hash($_[0]->get_language())->{'dll'};
 }
 
-sub get_template {
-  return 'wix';
+
+sub get_lib_template_input_file {
+  return $_[0]->get_info_hash($_[0]->get_language())->{'lib'};
 }
+
+
+sub get_template {
+  return $_[0]->get_info_hash($_[0]->get_language())->{'template'};
+}
+
 
 1;
