ac42f3ac3946e4a4cebb72f244b5f611e259b6e9
Adam Mitz <mitza-oci@users.noreply.github.com>
2011-04-27 14:32:46 +0000

    ChangeLogTag: Wed Apr 27 14:31:56 UTC 2011  Adam Mitz  <mitza@ociweb.com>

--- modules/MakeWorkspaceCreator.pm
+++ modules/MakeWorkspaceCreator.pm
@@ -61,7 +61,7 @@ sub write_comps {
   ## Send all the information to our base class method
   $self->write_named_targets($fh, $self->crlf(), \%targnum, \@list,
                              ($self->languageIs(Creator::csharp) ?
-                               'bundle ' : '') . $targets, '', 'generated ',
+                               'bundle ' : '') . $targets, '', '',
                              $self->project_target_translation(1), 1);
 }
 
