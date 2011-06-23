da55f2445c9d73bf67e38086bfad9dc5115b840b
johnnyw <johnnyw@a3e5c962-4219-0410-a828-e124f845ac39>
2011-06-23 11:48:23 +0000

    ChangeLogTag: Thu Jun 23 13:46:08 CEST 2011  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ tags/ACE+TAO+CIAO-6_0_3/modules/BorlandProjectBase.pm
@@ -0,0 +1,23 @@
+package BorlandProjectBase;
+
+# ************************************************************
+# Description   : A Borland base module for Borland Project Creators
+# Author        : Chad Elliott
+# Create Date   : 5/5/2009
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+use WinProjectBase;
+use BorlandPropertyBase;
+
+our @ISA = qw(BorlandPropertyBase WinProjectBase);
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+1;
