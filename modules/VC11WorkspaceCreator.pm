b03238719a45e68bc2a21fa637edb4cc95d647ae
Johnny Willemsen <jwillemsen@remedy.nl>
2011-12-12 19:04:36 +0000

    Mon Dec 12 19:03:08 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * modules/Options.pm:
            * modules/VC11ProjectCreator.pm:
            * modules/VC11WorkspaceCreator.pm:
              Added vc11 type, looks like the project format is still the
    same
              as with vc10, but this way we can use -type vc11 in some
    experimental
              builds

---
+++ modules/VC11WorkspaceCreator.pm
@@ -0,0 +1,22 @@
+package VC11WorkspaceCreator;
+
+# ************************************************************
+# Description   : A VC11 Workspace Creator
+# Author        : Johnny Willemsen
+# Create Date   : 12/12/2011
+# $Id$
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+
+use VC11ProjectCreator;
+use VC10WorkspaceCreator;
+
+use vars qw(@ISA);
+@ISA = qw(VC10WorkspaceCreator);
+
+1;
