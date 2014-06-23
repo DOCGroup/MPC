130a79b3f4a060ec3b713f26fcb0964874e2a582
johnnyw <johnnyw@a3e5c962-4219-0410-a828-e124f845ac39>
2014-06-23 07:44:48 +0000

    ChangeLogTag: Mon Jun 23 09:43:57 CEST 2014  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ tags/ACE+TAO+CIAO-6_2_7/modules/VC14ProjectCreator.pm
@@ -0,0 +1,57 @@
+package VC14ProjectCreator;
+
+# ************************************************************
+# Description   : A VC12 Project Creator
+# Author        : Johnny Willemsen
+# Create Date   : 10/29/2013
+# $Id$
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+
+use VC10ProjectCreator;
+
+use vars qw(@ISA);
+@ISA = qw(VC10ProjectCreator);
+
+## NOTE: We call the constant as a function to support Perl 5.6.
+my %info = (Creator::cplusplus() => {'ext'      => '.vcxproj',
+                                     'dllexe'   => 'vc14exe',
+                                     'libexe'   => 'vc14libexe',
+                                     'dll'      => 'vc14dll',
+                                     'lib'      => 'vc14lib',
+                                     'template' => [ 'vc10', 'vc10filters' ],
+                                    },
+           );
+
+my %config = ('vcversion' => '14.00',
+              'prversion' => '10.0.30319.1',
+              'toolsversion' => '4.0',
+              'targetframeworkversion' => '4.0',
+              'xmlheader' => 1
+              );
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+sub get_info_hash {
+  my($self, $key) = @_;
+
+  ## If we have the setting in our information map, the use it.
+  return $info{$key} if (defined $info{$key});
+
+  ## Otherwise, see if our parent type can take care of it.
+  return $self->SUPER::get_info_hash($key);
+}
+
+sub get_configurable {
+  my($self, $name) = @_;
+  return $config{$name};
+}
+
+1;
