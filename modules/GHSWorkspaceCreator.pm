4382624140b0ee01ab4452423061176fc7173b1e
Son Dinh <dinhs@objectcomputing.com>
2024-05-15 17:52:32 -0500

    Initial changes

--- modules/GHSWorkspaceCreator.pm
+++ modules/GHSWorkspaceCreator.pm
@@ -71,19 +71,44 @@ sub pre_workspace {
     }
   }
 
+  # TODO(sonndinh): Some platform information is configurable such as
+  # the path to an INTEGRITY installation, bsp name.
+  # Some other information is specific to ACE such as its root directory,
+  # its compilation and linking requirements (C++ version, flags, etc).
+  # These can be put in some form of input to MPC instead of hardcoded here.
+  # Update: We can use environment variables and command-line options:
+  # --expand_vars, -use_env. Or can use the -relative command-line option.
+  # The first option requires setting environment variables.
+  # The second option requires passing parameters to the -relative option.
+
   ## Print out the preliminary information
   print $fh "#!gbuild$crlf",
+            "macro __OS_DIR=C:\ghs\int1146$crlf",
+            "macro __BSP_NAME=sim800$crlf",
+            'macro __BSP_DIR=${__OS_DIR}\${__BSP_NAME}$crlf',
+            "macro ACE_ROOT=%expand_path(.)$crlf",
+            'macro __BUILD_DIR=${ACE_ROOT}\build$crlf',
+            'macro __LIBS_DIR_BASE=${__OS_DIR}\libs$crlf',
             "primaryTarget=$tgt$crlf",
+            'customization=${__OS_DIR}\target\integrity.bod$crlf',
             "[Project]$crlf",
-            "\t--one_instantiation_per_object$crlf",
+            #"\t-DACE_HAS_CPP14$crlf",
+            "\t-gcc$crlf",
+            "\t--c++14$crlf",
+            "\t--libcxx$crlf",
             "\t:sourceDir=.$crlf",
-            "\t--std$crlf",
-            "\t-language=cxx$crlf",
-            "\t--long_long$crlf",
-            "\t--new_style_casts$crlf";
+            '\t:optionsFile=${__OS_DIR}\target\${__BSP_NAME}.opt$crlf',
+	          '\t-I${ACE_ROOT}$crlf',
+	          "\t-language=cxx$crlf",
+	          "\t--new_style_casts$crlf",
+	          "\t-non_shared$crlf";
 }
 
-
+# TODO(sonndinh): Looks like this only support [INTEGRITY Application] with
+# only one [Program] (i.e., executable) in the image. But this seems sufficient
+# for most cases.
+# How does a [INTEGRITY Application] gpj file look with more
+# than one [Program]s and how does the corresponding .int file look?
 sub create_integrity_project {
   my($self, $int_proj, $project, $type, $target) = @_;
   my $outdir   = $self->get_outdir();
@@ -111,9 +136,9 @@ sub create_integrity_project {
                 "AddressSpace$crlf",
                 "\tFilename\t\t\t$target$crlf",
                 "\tLanguage\t\t\tC++$crlf",
-                "\tLibrary\t\t\t\tlibINTEGRITY.so$crlf",
-                "\tLibrary\t\t\t\tlibc.so$crlf",
-                "\tLibrary\t\t\t\tlibscxx_e.so$crlf",
+                #"\tLibrary\t\t\t\tlibINTEGRITY.so$crlf",
+                #"\tLibrary\t\t\t\tlibc.so$crlf",
+                #"\tLibrary\t\t\t\tlibscxx_e.so$crlf",
                 "\tTask Initial$crlf",
                 "\t\tStackLength\t\t0x8000$crlf",
                 "\tEndTask$crlf",
@@ -137,25 +162,34 @@ sub mix_settings {
     my $integrity_project = (index($tgt, 'integrity') >= 0);
     my($int_proj, $int_type, $target);
 
+    # (sonndinh): Go through the lines in this project's gpj file.
+    # Each line may result in some changes added to the workspace gpj file.
+    # The changes are returned in the $mix variable.
+    # In case the project is an 
     while(<$rh>) {
+      # (sonndinh): Don't need to add compiler/linker options to the workspace file.
+      # The gpj file for each individual project should have those already.
+      # In the workspace file (the top-level project file), only need to list the child projects.  
       if (/^\s*(\[(Program|Library|Subproject)\])\s*$/) {
         my $type = $1;
         if ($integrity_project && $type eq '[Program]') {
-          $int_proj = $project;
-          $int_proj =~ s/(\.gpj)$/_int$1/;
-          $int_type = $type;
-          $mix =~ s/(\.gpj)$/_int$1/;
-          $type = $integrity;
+          $int_proj = $project;             #E.g., tests/ARGV_Test.gpj
+          $int_proj =~ s/(\.gpj)$/_int$1/;  #E.g., tests/ARGV_Test_int.gpj
+          $int_type = $type;                #E.g., [Program]
+          $mix =~ s/(\.gpj)$/_int$1/;       #E.g., tests/ARGV_Test_int.gpj
+          $type = $integrity;               # [INTEGRITY Application]
         }
-        $mix .= "\t\t$type$crlf" .
-                "\t-object_dir=" . $self->mpc_dirname($project) .
-                '/.obj' . $crlf;
+        $mix .= "\t\t$type$crlf";
+        #$mix .= "\t\t$type$crlf" .
+        #        "\t-object_dir=" . $self->mpc_dirname($project) .
+        #        '/.obj' . $crlf;
       }
       elsif (/^\s*(\[Shared Object\])\s*$/) {
-        $mix .= "\t\t$1$crlf" .
-                "\t-pic$crlf" .
-                "\t-object_dir=" . $self->mpc_dirname($project) .
-                '/.shobj' . $crlf;
+        $mix .= "\t\t$1$crlf";
+        #$mix .= "\t\t$1$crlf" .
+        #        "\t-pic$crlf" .
+        #        "\t-object_dir=" . $self->mpc_dirname($project) .
+        #        '/.shobj' . $crlf;
       }
       elsif ($integrity_project && /^(.*\.bsp)\s/) {
         push(@integ_bsps, $1);
@@ -164,7 +198,7 @@ sub mix_settings {
         if (/^\s*\-((\w)\w*)/) {
           ## Save the required options into the mixed project string
           if (defined $directives{$2} || defined $directives{$1}) {
-            $mix .= $_;
+            #$mix .= $_;
           }
 
           ## If this is an integrity project, we need to find out
@@ -189,7 +223,17 @@ sub mix_settings {
 sub write_comps {
   my($self, $fh) = @_;
 
-  ## Print out each projet
+  #my $projects = $self->get_projects();
+  #my @list = $self->sort_dependencies($projects);
+
+  #foreach (@list) {
+  #  print "$_\n";
+  #}
+
+  # TODO(sonndinh): Looks like the individual project files are produced before this happens.
+  # And this module relies on them to add contents to the workspace file (default.gpj in case of GHS).
+
+  ## Print out each project
   foreach my $project ($self->sort_dependencies($self->get_projects(), 0)) {
     print $fh $self->mix_settings($project);
   }
